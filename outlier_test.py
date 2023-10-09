a447c9cb5d41161cd48a3b29502cc62efc5990e2
itswangbo <54987772+itswangbo@users.noreply.github.com>
2023-10-09 10:25:02 +0000

    Ver1.0

--- outlier_test.py
+++ outlier_test.py
@@ -5,7 +5,6 @@ import os
 import csv
 from queue import Queue
 
-
 LABEL = 0 # NOTE: to label each point
 N_INSTANCE = 0
 N_POINTS = 0
@@ -26,6 +25,7 @@ class Fully_Cluster():
         self.clusters = [set() for _ in range(t+1)]
 
         self.selected_centers = []
+        self.selected_clusters = []
         self.is_success = False
         self.true_radius = 0
 
@@ -51,7 +51,7 @@ class Fully_Cluster():
 
     def delete_current_center(self, cluster_index):
         deleted_points = []
-        for c_index in range(cluster_index, self.n_cluster):
+        for c_index in range(cluster_index, self.n_cluster+1):
             deleted_points.extend(list(self.clusters[c_index]))
             self.clusters[c_index].clear()
 
@@ -105,8 +105,8 @@ class Fully_Cluster():
                     s_relation[i].add(j)
                     s_relation[j].add(i)
 
-        selected_centers = []
-        selected_clusters = []
+        self.selected_centers = []
+        self.selected_clusters = []
 
         B_X = [0]*self.n_cluster
         for i in range(self.n_cluster):
@@ -114,14 +114,14 @@ class Fully_Cluster():
         W = set(range(self.n_cluster))
         n_covered_points = 0
 
-        while len(selected_centers)<=self.k and len(W)>0:
+        while len(self.selected_centers)<self.k and len(W)>0:
             B_hat_X = [0]*self.n_cluster
             for i in range(self.n_cluster):
                 B_hat_X[i] = B_X[i] # + B_hat_X[i]
                 for j in s_relation[i]:
                     B_hat_X[i] = B_hat_X[i]+B_X[j]
             selected_center_index, _ = max(enumerate(B_hat_X), key=lambda x: x[1]) # B_hat_X.index(max(B_hat_X))
-            selected_centers.append(ACTIVE_POINTS[self.centers[selected_center_index]])
+            self.selected_centers.append(self.centers[selected_center_index])
             selected_cluster = []
             for center_index in W:
                 dist = self.fully_distance(self.centers[center_index], self.centers[selected_center_index])
@@ -129,28 +129,27 @@ class Fully_Cluster():
                     selected_cluster.append(center_index)
                     n_covered_points = n_covered_points + B_X[center_index]
                     B_X[center_index] = 0
-            selected_clusters.append(selected_cluster)
+            self.selected_clusters.append(selected_cluster)
             for center in selected_cluster:
                 W.remove(center)
 
         self.is_success = True if len(ACTIVE_POINTS)-n_covered_points > (1+self.eps)*self.z else False
 
-    def fully_distance_point_index(self, f_point, s_point_index):
-        s_point = ACTIVE_POINTS[s_point_index]
-        print(f_point, s_point)
-        return math.sqrt(sum([(float(a)-float(b))**2 for (a, b) in list(zip(f_point, s_point))]))
-
     def fully_true_radius(self):
         dists = []
-        for point in ACTIVE_POINTS:
+        for p_index in ACTIVE_POINTS:
             dist = sys.float_info.max ## NOTE: this is the maximal float
-            for center_index in range(self.n_cluster):
-                d = self.fully_distance_point_index(point, center_index)
+            for center_index in self.selected_centers:
+                d = self.fully_distance(p_index, center_index)
                 dist = min(dist, d)
             dists.append(dist)
         dists.sort()
         self.true_radius = dists[-math.floor((1+self.eps)*self.z+1)] # NOTE: need to plus 1
 
+    def fully_get_centers(self):
+        return [ACTIVE_POINTS[center_index] for center_index in self.selected_centers]
+
+## BELOW IS THE MAIN PART
 
 def read_next_day_points(levels, file_path: str):
     global LABEL, ACTIVE_POINTS, POINTS_IN_DAYS, N_POINTS
@@ -161,8 +160,7 @@ def read_next_day_points(levels, file_path: str):
 
         points_in_the_day = []
         for row in csv_reader:
-            print(row)
-            cur_point = row[:-1]
+            cur_point = row[1:-1]
             ACTIVE_POINTS[LABEL] = cur_point
             N_POINTS = N_POINTS+1
             points_in_the_day.append(LABEL)
@@ -195,29 +193,26 @@ def fully_initialize_level_array(k, t, z, tau, eps, d_min, d_max):
 
 def get_centers(data_dir, s, k, t, z, tau, eps, d_min, d_max):
     levels = fully_initialize_level_array(k, t, z, tau, eps, d_min, d_max)
-    print(len(levels))
 
     file_names = os.listdir(data_dir)
     file_names = sorted(file_names)
 
     for file_name in file_names:
         file_path = os.path.join(data_dir, file_name)
-        print(file_path)
         read_next_day_points(levels, file_path)
         if POINTS_IN_DAYS.qsize() > s:
             delete_previous_day_points(levels)
         for level in levels:
-            print(level)
             level.fully_k_center_greedy()
-            level.fully_true_radius() ## NOTE: compute the true radius, just for testing, we can remove this line
+            level.fully_true_radius() ## NOTE: compute the true radius, just for testing, we can remove this line later
             if level.is_success is True:
                 print(level.true_radius)
-                return level.selected_centers
+                return level.fully_get_centers()
         print("ERROR!")
         return None
 
 
 if __name__ == "__main__":
-    result_centers = get_centers(data_dir ="./data", s=1000, k=2, t=200, z=10, tau=0.1, eps=0.01, d_min=0.1, d_max=10)
+    result_centers = get_centers(data_dir ="./data", s=1, k=2, t=100, z=9, tau=0.1, eps=0.1, d_min=0.1, d_max=10)
     print(result_centers)
           
